3221e43c8e2f4fd906e70bcc06496ed871421fa2
davm92 <davm92@hotmail.com>
2016-12-05 16:42:44 -0600

    primera entrega

---
+++ app.js
@@ -0,0 +1,8 @@
+(function(){ //llamamos la funcion para establecer nuestro global scope
+	'use strict'; //usaremos strict mode de js
+
+		.controller('entregaController',['$scope', '$http', function($scope, $http){
+
+		}]);
+
+})();
